7b9113173ac8482dc4634907ca93d7c73a139eb4
Chali-Anne <chali.anne.lauzon@gmail.com>
2017-11-21 07:05:32 -0500

    Init

---
+++ board.js
@@ -0,0 +1,11 @@
+board = (function () {
+    var _this = this;
+
+    _this.paint = function(){
+        document
+            .getElementById("app")
+            .innerHTML = "te2st";
+    }
+
+    return _this;
+})()
